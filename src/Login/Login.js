394b46dbae82de75fd4beba025669a3d9f8717c9
ilyount02 <qkddlfdud02@gmail.com>
2024-05-31 16:55:36 +0900

    x

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -5,28 +5,21 @@ import { useForm } from "react-hook-form";
 
 function Login(props) {
     const [state, setState] = useState({
-        email: "",
-        password: "",
+      email: "",
+      password: "",
     });
     const { register, handleSubmit } = useForm();
     const onSubmit = (data) => {
-        setState(data);
-        console.log(state);
+      setState(data);
+      console.log(state);
     };
-    console.log("렌더링");
-
+  
     return (
-            <form onSubmit={handleSubmit(onSubmit)} method="POST">
-                <input name="email" placeholder="Enger email" ref={register}/>
-
-                <input
-                    name="password"
-                    placeholder="Password"
-                    type="password"
-                    ref={register({ required: true })}
-                />
-                <input type="submit"/>
-            </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" name="email" {...register} />
+        <input type="password" name="password" {...register} />
+        <button type="submit">로그인</button>
+      </form>
     )
 }
 
